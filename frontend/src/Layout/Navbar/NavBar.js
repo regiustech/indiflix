b59aa2616b143cb23d843e77a9e620020bd42b4b
abhikhya2024 <abhikhya2024>
2024-05-02 14:16:58 +0530

    Profile page added

--- frontend/src/Layout/Navbar/NavBar.js
+++ frontend/src/Layout/Navbar/NavBar.js
@@ -91,7 +91,7 @@ function NavBar() {
             </NavLink> : <NavLink to="/login" className={Hover}>
               Login
             </NavLink>}
-            <NavLink to="/favorite" className={`${Hover} relative`}>
+            <NavLink to="/profile" className={`${Hover} relative`}>
               <FaHeart className="w-6 h-6" />
               <div className="w-5 h-5 flex-colo rounded-full text-xs bg-subMain text-white absolute -top-5 -right-1">
                 3
